8151158e6f0e348cc7d5c3bfe9ce78b6cd482f13
cjvil <christiejvillanueva@gmail.com>
2017-09-11 17:18:31 -0700

    Pass example data from app to video player
    Video url not yet working

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ angular.module('video-player')
 .component('app', {
   controller: function() {
     this.videos = exampleVideoData;
+    this.videoPlaying = exampleVideoData[0];
   },
   templateUrl: 'src/templates/app.html',
   
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -3,12 +3,13 @@ angular.module('video-player')
 .component('videoPlayer', {
   templateUrl: 'src/templates/videoPlayer.html',
 
-  // bindings: {
-  //   video: '<'
-  // },
+  bindings: {
+    videoPlaying: '<'
+  },
 
   controller: function() {
-    this.video = exampleVideoData[0];
-    console.log(this.video, 'this.video');
+    debugger;
+    // this.video = exampleVideoData[0];
+    console.log(this.videoPlaying, 'this.video');
   }
 });
